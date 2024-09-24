af86529717208427c2c65a082fdec58d886e73db
bthloo <basselalaa55@gmail.com>
2024-09-24 13:21:18 +0300

    Initial Commit

---
+++ Movie AppTests/Movie_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_AppTests.swift
+//  Movie AppTests
+//
+//  Created by Bthloo on 24/09/2024.
+//
+
+import Testing
+@testable import Movie_App
+
+struct Movie_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
