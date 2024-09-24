51d8b236da3529759ef1d66311669a4f40df599e
bthloo <basselalaa55@gmail.com>
2024-09-24 19:01:03 +0300

    Add Movies Screen

---
+++ Movie App/MovieCell.swift
@@ -0,0 +1,12 @@
+//
+//  MovieCell.swift
+//  Movie App
+//
+//  Created by Bthloo on 24/09/2024.
+//
+
+import UIKit
+
+class MovieCell: UICollectionViewCell {
+    
+}
---
+++ Movie App/MovieModel.swift
@@ -0,0 +1,8 @@
+//
+//  MovieModel.swift
+//  Movie App
+//
+//  Created by Bthloo on 24/09/2024.
+//
+
+import Foundation
